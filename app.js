cebd9adf52c09f1f17c3423506f8b6c87a0e63b5
R1zzo23 <adamjrizzo@yahoo.com>
2018-10-11 13:12:32 -0400

    Added buttons, idle worker functions

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
-document.addEventListener("DOMContentLoaded", function(){
+$(document).ready(function() {
+  stats = colony.stats;
+  skills = colony.skills;
+
+  elemLevel = document.getElementById("level");
+  elemTotalXp = document.getElementById('totalXp');
+  elemPopulation = document.getElementById('population');
+  elemIdle = document.getElementById('idle');
+  elemGold = document.getElementById('gold');
+  elemStone = document.getElementById('stone');
+  elemWheat = document.getElementById('wheat');
+  elemFish = document.getElementById('fish');
+  elemWood = document.getElementById('wood');
+  elemGoldWorkers = document.getElementById('goldWorkers');
+  elemStoneWorkers = document.getElementById('stoneWorkers');
+  elemWheatWorkers = document.getElementById('wheatWorkers');
+  elemFishWorkers = document.getElementById('fishWorkers');
+  elemWoodWorkers = document.getElementById('woodWorkers');
+
   showStats();
+  updateWorkerDistribution();
 });
 
 let colony = {
   stats : {
     "level" : 1,
     "population" : 3,
+    "idle" : 3,
     "totalXp" : 0,
-    "gold" : 0,
-    "stone" : 0,
-    "wheat" : 0,
-    "fish" : 0,
-    "wood" : 0,
+    gold : {
+      "amount" : 0,
+      "workers" : 0,
+    },
+    stone : {
+      "amount" : 0,
+      "workers" : 0,
+    },
+    wheat : {
+      "amount" : 25,
+      "workers" : 0,
+    },
+    fish : {
+      "amount" : 0,
+      "workers" : 0,
+    },
+    wood : {
+      "amount" : 25,
+      "workers" : 2,
+    },
   },
   skills : {
     mining : {
@@ -45,25 +80,36 @@ let colony = {
   }
 };
 
-let stats = colony.stats;
-let skills = colony.skills;
-
-let elemLevel = document.getElementById("level");
-/*let elemTotalXp = document.getElementById('totalXp');
-let elemPopulation = document.getElementById('population');
-let elemGold = document.getElementById('gold');
-let elemStone = document.getElementById('stone');
-let elemWheat = document.getElementById('wheat');
-let elemFish = document.getElementById('fish');
-let elemWood = document.getElementById('wood');*/
-
 function showStats() {
   elemLevel.textContent = stats.level;
-  /*elemTotalXp.textContent = stats.totalXp;
+  elemTotalXp.textContent = stats.totalXp;
   elemPopulation.textContent = stats.population;
-  elemGold.textContent = stats.gold;
-  elemStone.textContent = stats.stone;
-  elemWheat.textContent = stats.wheat;
-  elemFish.textContent = stats.fish;
-  elemWood.textContent = stats.wood;*/
+  elemGold.textContent = stats.gold.amount;
+  elemStone.textContent = stats.stone.amount;
+  elemWheat.textContent = stats.wheat.amount;
+  elemFish.textContent = stats.fish.amount;
+  elemWood.textContent = stats.wood.amount;
+}
+
+function makeVillagerIdle(x) {
+  var workersOnThisResource = stats[x].workers;
+
+  if (workersOnThisResource === 0){
+    console.log("There are no workers collecting " + x + "!");
+  }
+  else {
+    stats[x].workers--;
+    stats.idle++;
+    updateWorkerDistribution();
+    console.log("Reduce " + x + " workers by 1!");
+  }
+}
+
+function updateWorkerDistribution() {
+  elemIdle.textContent = stats.idle;
+  elemGoldWorkers.textContent = stats.gold.workers;
+  elemStoneWorkers.textContent = stats.stone.workers;
+  elemWheatWorkers.textContent = stats.wheat.workers;
+  elemFishWorkers.textContent = stats.fish.workers;
+  elemWoodWorkers.textContent = stats.wood.workers;
 }
