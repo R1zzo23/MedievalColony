7594cb0752c51a5ac533b2f668dba8659f5d0858
R1zzo23 <adamjrizzo@yahoo.com>
2018-10-13 06:42:50 -0400

    Added XP to colony and each skill at end of day

--- endOfDay.js
+++ endOfDay.js
@@ -1,27 +1,33 @@
 function endDay() {
   if (stats.gold.workers > 0) {
-    calculateResourcesCollected("gold", "mining");
+    addSkillXp(calculateResourcesCollected("gold", "mining"), "mining");
   }
   if (stats.stone.workers > 0) {
-    calculateResourcesCollected("stone", "mining");
+    addSkillXp(calculateResourcesCollected("stone", "mining"), "mining");
   }
   if (stats.fish.workers > 0) {
-    calculateResourcesCollected("fish", "fishing");
+    addSkillXp(calculateResourcesCollected("fish", "fishing"), "fishing");
   }
   if (stats.wheat.workers > 0) {
-    calculateResourcesCollected("wheat", "farming");
+    addSkillXp(calculateResourcesCollected("wheat", "farming"), "farming");
   }
   if (stats.wood.workers > 0) {
-    calculateResourcesCollected("wood", "lumber");
+    addSkillXp(calculateResourcesCollected("wood", "lumber"), "lumber");
   }
   showStats();
 }
 
 function calculateResourcesCollected(resource, skill) {
-  let amountCollected = (stats[resource].workers * 20) * (skills[skill].level * 0.5);
-  stats[resource].amount += Math.round(amountCollected);
+  let amountCollected = Math.round((stats[resource].workers * 20) * (skills[skill].level * 0.5));
+  stats[resource].amount += amountCollected;
+  return amountCollected;
 }
 
-function addSkillXp(skill) {
-  
+function addSkillXp(collected, skill) {
+  let xpAdded = Math.floor(Math.random() * ((collected * 0.75) - (collected * 0.3) + 1)) + (collected * 0.3);
+  console.log('xpAdded: ' + xpAdded);
+  skills[skill].xp += xpAdded;
+  console.log(skill + ' total XP now: ' + skills[skill].xp);
+  stats.totalXp += xpAdded;
+  console.log('Total Colony XP Now: ' + stats.totalXp);
 }
